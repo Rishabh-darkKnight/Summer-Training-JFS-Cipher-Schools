1e2a6f3ce516acb45146d0c323c2f72d39fd3b38
Rishabh Kumar <90150037+Rishabh-darkKnight@users.noreply.github.com>
2023-06-08 10:44:10 +0530

    Add files via upload

---
+++ Day1/Basic.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Basic {
+    public static void main(String[] args) {
+        // Printing output
+        System.out.println("Ready to go !!");
+        // Input taking through Scanner class
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Square of number :- " + n*n);
+    }
+}
